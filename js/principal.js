44de49c3def2237ae2d9f6571fc515c7d93078dc
WMAlves <williamma03@gmail.com>
2022-07-21 22:03:38 -0300

    2º Commit
    Intodução do curso.

---
+++ js/principal.js
@@ -0,0 +1,7 @@
+
+    console.log("Fui carregado de um arquivo externo");
+				var titulo = document.querySelector(".titulo");
+				console.log(titulo);
+				console.log(titulo.textContent);
+				titulo.textContent = "Aparecida Nutricionista";
+				
