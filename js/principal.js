f741832f9c4cf0b901f8bd89c50ad65e11517530
WMAlves <williamma03@gmail.com>
2022-07-25 13:33:23 -0300

    1º Commit do trabalho
    Necessário estudar e verificar por que a altura não está sendo validada no seu if, há algum erro besta.
    
    verificar!!!
    Aula 7
    Link: https://cursos.alura.com.br/course/javascript-programando-na-linguagem-web/task/24668

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,48 @@
 
-    console.log("Fui carregado de um arquivo externo");
-				var titulo = document.querySelector(".titulo");
+    				var titulo = document.querySelector(".titulo");
 				console.log(titulo);
 				console.log(titulo.textContent);
-				titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
 				
+//console.log(paciente);
+//console.log(tdPeso);
+//console.log(peso);
+//console.log(altura);
+
+tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+
+var alturaEhValida = true;
+
+if (peso < 0 || peso > 100) {
+	console.log("Peso invalido");
+	pesoEhValido = false;
+	tdImc.textContent = ("Peso Invalido");
+}
+
+if (altura < 0 || altura > 3,00) {
+	console.log("Altura invalida");
+	alturaEhValida = false;
+	tdImc = ("Altura invalida");
+}
+
+	if (pesoEhValido && alturaEhValida) {
+		var imc = peso / (altura * altura);
+		tdImc.textContent = imc;
+
+	}
+
+//https://cursos.alura.com.br/course/javascript-programando-na-linguagem-web/task/24668
+//verificar pq o calculo de altura não está correto.
+
+
