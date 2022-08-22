d8cd0903063e3518d19fb9547b16382270f9cfcd
WMAlves <williamma03@gmail.com>
2022-08-22 13:15:58 -0300

    16º Commit
    Correção do filtro de pacientes.

--- js/filtra.js
+++ js/filtra.js
@@ -2,34 +2,30 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function () {
     console.log(this.value);
-
-    var pacientes = document.querySelectorAll(".paciente");
-
-    
+    var pacientes = document.querySelectorAll(".paciente");   
 
 
+    if (this.value.length > 0) {
         for (var i = 0; i < pacientes.length; i++) {
-            var paicente = pacientes[i];
+            var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
-            var nome = paciente.textContent;
+            var nome = tdNome.textContent;
+            if (nome != this.value) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
 
-            //if (nome != this.value) {
-            //    paicente.classList.add("invisivel");
-            //} else {
-            //    paicente.classList.remove("invisivel");
-            //}
-            // necessário continuar daqui: 
-            // https://cursos.alura.com.br/course/javascript-programando-na-linguagem-web/task/24465
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
 
-
-       
-        
+        }
     }
-
-   
-
-
-
     
+        //https://cursos.alura.com.br/course/javascript-programando-na-linguagem-web/task/24465
+    //esconder os demais pacientes.
 
 });
