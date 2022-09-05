d0ab326441b9cd747dc71e47cd4ea9ef121a9387
WMAlves <williamma03@gmail.com>
2022-09-05 13:06:42 -0300

    Aula Final do Curso
    Feito o tratamento de erro de requisição.

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -8,15 +8,19 @@ botaoAdicionar.addEventListener("click", function () {
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function () {
-
-        var resposta = xhr.responseText;
-      
-        var pacientes = JSON.parse(resposta);
-
-        pacientes.forEach(function (paciente) {
-
-            adicionaPacienteNaTabela(paciente);
-        });
+        var erroAjax = document.querySelector("#erro_ajax");
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log();           
+            erroAjax.classList.remove("invisivel");
+        }
 
 
 
